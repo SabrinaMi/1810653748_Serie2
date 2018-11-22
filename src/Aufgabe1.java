2de889c91433d6d86082c82caee2f72f39a55e18
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-22 10:10:19 +0100

    Aufgabe1

--- src/Aufgabe1.java
+++ src/Aufgabe1.java
@@ -1,2 +1,22 @@
-public class Aufgabe1 {
+import javax.swing.*;
+
+public class Aufgabe1
+{
+    public static void main(String[] args)
+    {
+        String name;
+        name =JOptionPane.showInputDialog(null, "Bitte geben Sie Ihren Namen ein!");
+        int alter;
+        alter = Integer.parseInt(JOptionPane.showInputDialog(null,"Bitte gib mir dein Alter ein!"));
+
+        if (alter>= 18)
+        {
+           JOptionPane.showMessageDialog(null, name+" du bist volljährig!");
+        }
+        else
+            {
+                JOptionPane.showMessageDialog(null, name+" du bist nicht volljährig!");
+            }
+
+    }
 }
