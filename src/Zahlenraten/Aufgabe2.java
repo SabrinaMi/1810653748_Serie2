cea62541e7d3a1cc9383da3fdba1110287f00779
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-22 11:13:26 +0100

    Aufgabe2

---
+++ src/Zahlenraten/Aufgabe2.java
@@ -0,0 +1,24 @@
+package Zahlenraten;
+
+import javax.swing.*;
+import java.util.Random;
+
+public class Aufgabe2<zufallszahl, gerade>
+{
+    public static void main(String[] args) {
+        int min = 1;     //
+        int max = 10;
+        Random rnd = new Random();
+        int zufallszahl = rnd.nextInt(max - min + 1) + min;
+        System.out.println(zufallszahl);
+        /* mit dem Code wird angegeben von welcher Zahl aus, der Rechner starten muss und mit welcher Zahl er enden
+        muss und mit dem rnd.nextInt wird gesagt, dass er es per Zufallsgenerator erledigen soll*/
+
+        if ((zufallszahl%2)==0)
+        {
+            JOptionPane.showMessageDialog(null, zufallszahl + " ist gerade!");
+        } else {
+            JOptionPane.showMessageDialog(null, zufallszahl + " ist nicht gerade!");
+        }
+    }
+}
