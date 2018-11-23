a9ecdd46760d10e8e49e98f4fbf120b2cd04c5fa
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-23 14:38:59 +0100

    Aufgabe6

--- src/Zahlenraten/Aufgabe5.java
+++ src/Zahlenraten/Aufgabe5.java
@@ -12,28 +12,56 @@ public class Aufgabe5 {
         int zufallszahl = rnd.nextInt(max - min + 1) + min;
         int i = 0;
 
-        while (i < 3) {
-         String rueckgabe;
+        //while (i < 3) {
+         //String rueckgabe;
 
-        rueckgabe = JOptionPane.showInputDialog(null, " gib mir bitte eine Ganzzahl, die zwischen 1 und 10 liegt");
+        //rueckgabe = JOptionPane.showInputDialog(null, " gib mir bitte eine Ganzzahl, die zwischen 1 und 10 liegt");
 
-            if (Integer.parseInt(rueckgabe) == zufallszahl) {
-                JOptionPane.showMessageDialog(null, "Deine Zahl ist gleich");
+           // if (Integer.parseInt(rueckgabe) == zufallszahl) {
+               // JOptionPane.showMessageDialog(null, "Deine Zahl ist gleich");
+                //break;
+               // } else {
+
+                //i++;
+
+               // if (i > 2) {
+
+                //     JOptionPane.showMessageDialog(null,"keine Übereinstimmung mit der Zufallszahl: " + zufallszahl + "! Alle Versuche aufgebraucht!");
+
+                //    } else {
+
+                     //JOptionPane.showMessageDialog(null,"keine übereinstimmung mit der Zufallszahl.noch " + (3 - i) + " Versuche übrig.");
+                   // }
+                //}
+        //4}
+        do{
+
+            int zz2 = Integer.parseInt(JOptionPane.showInputDialog(null, "gebe eine Zahl zwischen 1 und 10 ein"));
+
+            if (zz2 == zufallszahl) {
+
+                JOptionPane.showMessageDialog(null,"Zahl erraten");
+
                 break;
-                } else {
+
+            } else {
 
                 i++;
 
                 if (i > 2) {
 
-                     JOptionPane.showMessageDialog(null,"keine Übereinstimmung mit der Zufallszahl: " + zufallszahl + "! Alle Versuche aufgebraucht!");
+                    JOptionPane.showMessageDialog(null, "keine Übereinstimmung mit der Zufallszahl: " + zufallszahl + "! Alle Versuche aufgebraucht!");
 
-                    } else {
+                } else {
 
-                     JOptionPane.showMessageDialog(null,"keine übereinstimmung mit der Zufallszahl.noch " + (3 - i) + " Versuche übrig.");
-                    }
+                    JOptionPane.showMessageDialog(null, "keine übereinstimmung mit der Zufallszahl.noch " + (3 - i) + " Versuche übrig.");
+
                 }
-        }
+
+            }
+
+        }while(i<3);
+
 
 
     }
