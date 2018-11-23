a418c1bdca0cb0c0f11907e2b5d47ea1f193621f
Sabrina Mitternoeckler <1810653748@stud.fh-kufstein.ac.at>
2018-11-23 14:23:02 +0100

    Aufgabe5

---
+++ src/Zahlenraten/Aufgabe5.java
@@ -0,0 +1,40 @@
+package Zahlenraten;
+
+import javax.swing.*;
+import java.util.Random;
+
+public class Aufgabe5 {
+    public static void main(String[] args) {
+        int min = 1;
+        int max = 10;
+
+        Random rnd = new Random();
+        int zufallszahl = rnd.nextInt(max - min + 1) + min;
+        int i = 0;
+
+        while (i < 3) {
+         String rueckgabe;
+
+        rueckgabe = JOptionPane.showInputDialog(null, " gib mir bitte eine Ganzzahl, die zwischen 1 und 10 liegt");
+
+            if (Integer.parseInt(rueckgabe) == zufallszahl) {
+                JOptionPane.showMessageDialog(null, "Deine Zahl ist gleich");
+                break;
+                } else {
+
+                i++;
+
+                if (i > 2) {
+
+                     JOptionPane.showMessageDialog(null,"keine Übereinstimmung mit der Zufallszahl: " + zufallszahl + "! Alle Versuche aufgebraucht!");
+
+                    } else {
+
+                     JOptionPane.showMessageDialog(null,"keine übereinstimmung mit der Zufallszahl.noch " + (3 - i) + " Versuche übrig.");
+                    }
+                }
+        }
+
+
+    }
+}
